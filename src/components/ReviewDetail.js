202eba15a4d0b4d4bae3b3a872dc04e0b73d1926
Jason Chan <jchan321738@gmail.com>
2021-06-24 11:05:07 -0700

    remove delete review button

--- src/components/ReviewDetail.js
+++ src/components/ReviewDetail.js
@@ -7,9 +7,9 @@ const ReviewDetails = ({ id, body, user, created_at, deleteReview, currentUser }
                 <h5>{body}</h5> <br/>
                 <p>Created at {created_at.toLocaleString()}</p>
                 <p>By {user? user.first_name + " " + user.last_name : null}</p>
-                {/* { currentUser? ( */}
+                { currentUser? (
                 <button className="btn btn-danger" onClick={() => deleteReview(id)}>Delete</button>
-                {/* ) : "" }  */}
+                ) : "" } 
             </div>
         </div>
     )
